137b20861260d1c71de03a27c94bf787470a4e3c
Faizul Islam <152944531+FaizzE@users.noreply.github.com>
2024-08-07 15:53:29 +0530

    Update index.js

--- JavaScript/JS_Basics/index.js
+++ JavaScript/JS_Basics/index.js
@@ -11,3 +11,15 @@ console.log(lastName);
 lastName = 3;
 console.log(lastName);
 
+let num = 2;
+
+switch(num) {
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
+
